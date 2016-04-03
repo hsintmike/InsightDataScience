625af6d5a9dedee6e46138f2dbde2420a90e63b5
hsintmike <hsinjutung@gmail.com>
2016-04-02 22:06:07 -0700

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 # Assume having python install on local machine
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
 
+# add --print at the end of command line if you want to print out instantaneous value on the screen
+
 python ./src/average_degree.py ./tweet_input/tweets.txt ./tweet_output/output.txt
 
 
